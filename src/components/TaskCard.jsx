5e2073555e98a4bf2c3e54a37a337a71ab4ffa44
CralpCode <clp64413@gmail.com>
2022-09-26 20:40:38 -0600

    Mejoras Visuales

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -9,7 +9,7 @@ function TaskCard({ tasks }) {
     <div key={tasks.id} className="bg-gray-800 text-white p-4 rounded-md">
       <h1 className='text-xl font-bold capitalize'>{tasks.title}</h1>
       <p className='text-gray-500 text-sm'>{tasks.description}</p>
-      <button className='bg-red-500 py-1 rounded-md mt-4 hover:bg-red-400' onClick={() => deleteTask(tasks.id)}>
+      <button className='bg-red-500 py-1 rounded-md mt-4 hover:bg-red-400 p-4 font-bold' onClick={() => deleteTask(tasks.id)}>
         Eliminar
       </button>
     </div>
--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -1,5 +1,5 @@
-import {useState, useContext} from 'react'
-import {TaskContext} from '../context/TaskContext'
+import { useState, useContext } from 'react'
+import { TaskContext } from '../context/TaskContext'
 
 
 function TaskForm() {
@@ -7,12 +7,12 @@ function TaskForm() {
     const [title, settitle] = useState('')
 
     const [description, setdescription] = useState('')
-    
-    const {createTask} = useContext(TaskContext)
+
+    const { createTask } = useContext(TaskContext)
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        
+
         createTask(title, description)
         settitle('')
         setdescription('')
@@ -20,18 +20,18 @@ function TaskForm() {
 
     return (
         <div className='max-w-md mx-auto'>
-            <form onSubmit={handleSubmit} className='bg-slate-800 p-10 mb-4' >
+            <form onSubmit={handleSubmit} className='bg-slate-800 p-10 mb-4 rounded-md' >
                 <h1 className='text-2xl font-bold text-white mb-3'>Crea tu tarea</h1>
-                <input placeholder="Ecribe tu tarea" onChange={(e) => 
-                        settitle(e.target.value)
-                } 
-                value={title}
-                className='bg-slate-300 p-3 w-full mb-2'
+                <input placeholder="Ecribe tu tarea" onChange={(e) =>
+                    settitle(e.target.value)
+                }
+                    value={title}
+                    className='bg-slate-300 p-3 w-full mb-2 rounded-md'
                 />
                 <textarea placeholder='Escribe la decripcion de la tarea' onChange={(e) =>
-                  setdescription(e.target.value)  
-                } value={description} className='bg-slate-300 p-3 w-full mb-2'></textarea>
-                <button className='bg-indigo-500 py-1 text-white rounded-md p-4'>
+                    setdescription(e.target.value)
+                } value={description} className='bg-slate-300 p-3 w-full mb-2 rounded-md'></textarea>
+                <button className='bg-indigo-500 py-1 text-white rounded-md p-4 font-bold'>
                     Guardar
                 </button>
             </form>
